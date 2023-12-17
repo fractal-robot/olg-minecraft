e389b0fb606dfb8456e52801742931a586f65250
fractal-robot <utsdxbur@proton.me>
2023-12-17 19:06:09 +0100

    refactoring chunck manager logic and starting doing perlin noise generation

--- src/ChunkManager.cpp
+++ src/ChunkManager.cpp
@@ -28,34 +28,26 @@ void World::update(const glm::vec3 &pPos, Shader &shader) {
   // using eucledian distance formula to determine if the chunck should be
   // created/removed/rendered
 
+  std::vector<glm::ivec3> removeList;
+
   for (int x{-constants::viewDistance}; x < constants::viewDistance; ++x) {
     for (int z{-constants::viewDistance}; z < constants::viewDistance; ++z) {
       if (std::sqrt(std::pow(x * constants::chunkSize - pPos.x, 2) +
                     std::pow(z * constants::chunkSize - pPos.z, 2)) <=
-              constants::viewDistance &&
-          world.find({x, 0, z}) == world.end()) {
-        world[{x, 0, z}] = new Chunk;
+          constants::viewDistance) {
+        if (world.find({x, 0, z}) == world.end()) {
+          world[{x, 0, z}] = new Chunk;
+        }
+        glm::mat4 model{glm::mat4(1.0f)};
+        model = glm::translate(model, glm::vec3(x * constants::chunkSize,
+                                                0 * constants::chunkSize,
+                                                z * constants::chunkSize));
+        shader.setMatrix4("model", model);
+        world[{x, 0, z}]->render();
+      } else {
+        delete world[{x, 0, z}];
+        removeList.push_back({x, 0, z});
       }
-    }
-  }
-
-  std::vector<glm::ivec3> removeList;
-
-  for (const auto &e : world) {
-    if (std::sqrt(std::pow(e.first.x * constants::chunkSize - pPos.x, 2) +
-                  std::pow(e.first.z * constants::chunkSize - pPos.z, 2)) <=
-        constants::viewDistance) {
-
-      glm::mat4 model{glm::mat4(1.0f)};
-      model =
-          glm::translate(model, glm::vec3(e.first.x * constants::chunkSize,
-                                          e.first.y * constants::chunkSize,
-                                          e.first.z * constants::chunkSize));
-      shader.setMatrix4("model", model);
-      e.second->render();
-    } else {
-      delete e.second;
-      removeList.push_back(e.first);
     }
   }
 
--- src/constants.h
+++ src/constants.h
@@ -7,15 +7,15 @@ namespace constants {
 inline constexpr int windowWidth{1920};
 inline constexpr int windowHeight{1080};
 
-inline constexpr float cameraSpeed{24.f};
+inline constexpr float cameraSpeed{240.f};
 inline constexpr float cameraSensivity{.04f};
-inline constexpr float cameraAltitudeSpeed{22.f};
-inline constexpr int viewDistance{400};
+inline constexpr float cameraAltitudeSpeed{220.f};
+inline constexpr int viewDistance{200};
 
 inline constexpr int blockVerticesCount{120};
 inline constexpr int blockIndicesCount{36};
 
-inline constexpr int chunkSize{60};
+inline constexpr int chunkSize{4};
 
 inline constexpr int elementOffset{5};
 } // namespace constants
